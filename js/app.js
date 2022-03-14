5a2e51eb0da7ef73528170ee58bd41fc4f6cb5d9
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2022-03-14 08:18:42 +0800

    Update app.js
    Made code useful by adding an event listener when the extension is not running at the start of the code.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('I am running!');
+window.addEventListener('DOMContentLoaded', (event) => {
+    var extNotLoaded;
+    fetch("chrome-extension://" + extensionID + "/manifest.json")
+        .then(response => response.json())
+        .then(data => {
+            installedExtVersion = data.version;
+			extUpdateUrl = data.update_url;
+            extNotLoaded = setTimeout(() => {
+                document.getElementById('frame').src += ''
+            }, 1000);
+        })
+        .catch((error) => modal.classList.add('show'))
+		
+	if (window.navigator.windowControlsOverlay && window.navigator.windowControlsOverlay.visible) {
+		document.getElementsByName('theme-color')[0].setAttribute("content","#36393f");
+	}
+
+ 
