8788a0b370e1f2a8b13b2865264af1918fca4439
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2022-03-20 18:46:31 +0800

    Update sw.js
    changed version number.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/iframe-browser-pwa';
 var APP_PREFIX = 'iframebrowser_';
-var VERSION = 'version_004';
+var VERSION = 'version_005';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
