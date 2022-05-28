5ae185520b9143d9d2af8228ace59faf430a744b
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2022-05-28 13:04:23 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 console.log('Iframe browser am running!');
+// listen to the service worker promise in index.html to see if there has been a new update.
+// condition: the service-worker.js needs to have some kind of change - e.g. increment CACHE_VERSION.
+window['isUpdateAvailable']
+	.then(isAvailable => {
+		if (isAvailable) {
+			const toast = this.toastCtrl.create({
+				message: 'New Update available! Reload the webapp to see the latest juicy changes.',
+				position: 'bottom',
+				showCloseButton: true,
+			});
+			toast.present();
+		}
+	});
