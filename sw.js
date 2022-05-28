b9bb05fd00a11fab5077d7ac3fc55ff211ce7a90
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2022-05-28 13:14:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,6 +33,49 @@ self.addEventListener('install', function (e) {
     })
   )
 })
+"use strict";
+
+var SW_VER = 'SW0001';
+
+self.oninstall = event => {
+	console.log(SW_VER + ': oninstall');
+	event.waitUntil(caches.open(SW_VER)
+		.then(cache => {
+				return cache.addAll([
+					'index.html',
+					'index.js',
+					'sw.js',
+				]);
+		})
+	);
+}
+
+self.onactivate = event => {
+	console.log(SW_VER + ': onactivate');
+	event.waitUntil(caches.keys()
+		.then(keys => {
+			return Promise.all(keys.map(key => {
+				if (key != SW_VER) return caches.delete(key);
+			}));
+		})
+		.then(() => {
+			return self.clients.claim();
+		})
+	);
+}
+
+self.onfetch = event => {
+	console.log(SW_VER + ': onfetch = ' + event.request.url);
+	event.respondWith(caches.match(event.request)
+		.then(cached => cached || fetch(event.request))
+	);
+}
+
+self.onmessage = event => {
+	console.log(SW_VER + ': onmessage = ' + event.data);
+	if (event.data === 'skip-waiting') return skipWaiting();
+	if (event.data === 'get-version') return event.ports[0].postMessage(SW_VER);
+}
 
 self.addEventListener('activate', function (e) {
   e.waitUntil(
