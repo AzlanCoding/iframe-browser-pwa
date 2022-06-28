8b106410435380f570460c57a8e680f7ae0c2388
AzlanCoding <101229146+AzlanCoding@users.noreply.github.com>
2022-06-28 19:38:47 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,3 @@
-"use strict";
-
-var SW_VER = 'SW0003';
-
-self.oninstall = event => {
-	console.log(SW_VER + ': oninstall');
-	event.waitUntil(caches.open(SW_VER)
-		.then(cache => {
-				return cache.addAll([
-					'index.html',
-					'index.js',
-					'sw.js',
-				]);
-		})
-	);
-}
-
-self.onactivate = event => {
-	console.log(SW_VER + ': onactivate');
-	event.waitUntil(caches.keys()
-		.then(keys => {
-			return Promise.all(keys.map(key => {
-				if (key != SW_VER) return caches.delete(key);
-			}));
 var GHPATH = '/iframe-browser-pwa';
 var APP_PREFIX = 'iframebrowser_';
 var VERSION = 'version_008';
@@ -54,6 +30,23 @@ self.addEventListener('install', function (e) {
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('Installing cache : ' + CACHE_NAME);
       return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME);
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('Deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i])
+        }
+      }))
     })
   )
 })
